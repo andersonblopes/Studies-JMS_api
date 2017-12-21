218c28f390e4bba133c0ddb9b2e9cf1153a6ea62
andersonblopes <anderson.lopes.2010@gmail.com>
2017-12-21 19:34:44 -0300

    Recebendo mensagens com MessageListener

--- src/br/com/lopes/jms/TesteConsumidor.java
+++ src/br/com/lopes/jms/TesteConsumidor.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
 public class TesteConsumidor {
@@ -25,9 +28,25 @@ public class TesteConsumidor {
 
 		Destination destination = (Destination) context.lookup("financeiro");
 		MessageConsumer consumer = session.createConsumer(destination);
-		Message message = consumer.receive();
 
-		System.out.println("Recebendo mensagem: " + message);
+		// Read only one message
+		// Message message = consumer.receive();
+
+		// Read all messages
+		consumer.setMessageListener(new MessageListener() {
+
+			@Override
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+
+				try {
+					System.out.println("Recebendo mensagem: " + textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
 
 		new Scanner(System.in).nextLine();
 
