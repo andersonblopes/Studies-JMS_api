1f4765b984eb40347ee7affc4f5431599046284f
andersonblopes <anderson.lopes.2010@gmail.com>
2017-12-22 08:33:58 -0300

    Cria assinatura

--- src/br/com/lopes/jms/topic/TesteConsumidorTopico.java
+++ src/br/com/lopes/jms/topic/TesteConsumidorTopico.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.jms.Topic;
 import javax.naming.InitialContext;
 
 public class TesteConsumidorTopico {
@@ -22,11 +22,14 @@ public class TesteConsumidorTopico {
 		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
 
 		Connection connection = factory.createConnection();
+		connection.setClientID("estoque");
+
 		connection.start();
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-		Destination topico = (Destination) context.lookup("loja");
-		MessageConsumer consumer = session.createConsumer(topico);
+		Topic topico = (Topic) context.lookup("loja");
+
+		MessageConsumer consumer = session.createDurableSubscriber(topico, "assinatura");
 
 		consumer.setMessageListener(new MessageListener() {
 
