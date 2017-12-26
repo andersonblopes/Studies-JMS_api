c32c57859a8a5486cd019b1cc3b674ec135e0baa
andersonblopes <anderson.lopes.2010@gmail.com>
2017-12-26 20:32:50 -0300

    Inclui classes de modelo para gerar XML através do JAXB

--- src/br/com/lopes/jms/topic/TestaProdutorTopico.java
+++ src/br/com/lopes/jms/topic/TestaProdutorTopico.java
@@ -1,5 +1,7 @@
 package br.com.lopes.jms.topic;
 
+import java.io.StringWriter;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
@@ -7,6 +9,10 @@ import javax.jms.Message;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.naming.InitialContext;
+import javax.xml.bind.JAXB;
+
+import br.com.lopes.jms.modelo.Pedido;
+import br.com.lopes.jms.modelo.PedidoFactory;
 
 public class TestaProdutorTopico {
 
@@ -15,15 +21,24 @@ public class TestaProdutorTopico {
 		InitialContext context = new InitialContext();
 		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
 
-		Connection connection = factory.createConnection("admin","admin");
+		Connection connection = factory.createConnection("user", "user");
 		connection.start();
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
 		Destination topico = (Destination) context.lookup("loja");
 		MessageProducer producer = session.createProducer(topico);
 
-		Message message = session.createTextMessage("<pedido><id>222</id><e-book>true</e-book></pedido>");
-		//message.setBooleanProperty("ebook", true);
+		// Gera xml a partir de objeto
+		Pedido pedido = new PedidoFactory().geraPedidoComValores();
+		StringWriter writer = new StringWriter();
+		JAXB.marshal(pedido, writer);
+
+		// Gera string a partir de xml
+		String xml = writer.toString();
+		System.out.println(xml);
+
+		Message message = session.createTextMessage(xml);
+		// message.setBooleanProperty("ebook", true);
 		producer.send(message);
 
 		session.close();
